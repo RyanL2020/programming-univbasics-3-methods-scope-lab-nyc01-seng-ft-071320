21040b57500e2320a4df58a2698c97902dade8ff
Ryan Locascio <loc.fitness1@gmail.com>
2020-06-16 13:37:10 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,3 +13,12 @@ def link
  phrase = "It's Dangerous To Go Alone! Take This."
  puts phrase
  end 
+
+ def all_phrases
+   catch_phrases_mario = "It's-a me, Mario!"
+   catch_phrases_toadstool = 'Thank You Mario! But Our Princess Is In Another Castle!'
+   catch_phrases_link = "It's Dangerous To Go Alone! Take This."
+   puts catch_phrases_mario
+   puts catch_phrases_toadstool
+   puts catch_phrases_link
+ end
